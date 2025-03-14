6a7a84f224bb0d597181a893d29e93a8b830f46c
cana_trust <youremail@example.com>
2025-03-14 16:26:23 +0700

    new

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
+import { hostname } from 'os';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+    images: {
+
+        // domains: ['https://img.daisyui.com'],
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "**"
+            }
+        ]
+    },
+};
 
 export default nextConfig;
